8b52a899970d7b386064441009092c6bb2c5d063
JouiMontilivi <154421356+JouiMontilivi@users.noreply.github.com>
2024-01-19 20:52:08 +0100

    Update contador.php

--- contador.php
+++ contador.php
@@ -1,11 +1,12 @@
 <?php
 
-  $count=$_COOKIE[ 'count' ];
-  if(isset($count)){
+$count = $_COOKIE['count'];
+if (isset($count)) {
     $count++;
-  }else{
-    $count=0;
-  setcookie( 'count' ,$count);
-  echo $count;
+} else {
+    $count = 0;
+}
+setcookie('count', $count);
+echo $count;
 
 ?>
