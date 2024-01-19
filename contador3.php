22fda00be853a50ece63cdb8c5d762be9a323604
JouiMontilivi <154421356+JouiMontilivi@users.noreply.github.com>
2024-01-19 21:42:22 +0100

    Update contador3.php

--- contador3.php
+++ contador3.php
@@ -2,15 +2,13 @@
 
 $contador = 0;
 
-// Intenta obrir el fitxer en mode lectura
 if ($fd = fopen('counter.txt', 'r')) {
-    $contador = intval(fgets($fd)); // Converteix la cadena a un enter
+    $contador = fgets($fd);
     fclose($fd);
 }
 
-// Intenta obrir el fitxer en mode escriptura
 $fd2 = fopen('counter.txt', 'w');
-$contador = $contador + 1; // Incrementa el contador en 1
+$contador = $contador + 1;
 
 fwrite($fd2, $contador);
 fclose($fd2);
